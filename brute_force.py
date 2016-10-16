6474558d0eeaf94650c7813cff6a8c630baf9d1b
pbsinclair42 <bobsmith0030+git2@gmail.com>
2016-10-16 15:51:36 +0100

    Add PosSLP functionality to Analyser

--- brute_force.py
+++ brute_force.py
@@ -1,24 +1,26 @@
 from copy import deepcopy
 
-from slp import SLP
+from slp import SLP, PosSLP
 from utils import Line
 
 
 class Analyser:
 
-    def __init__(self):
-        self.slps = [{SLP()}]
+    def __init__(self, pos=False):
+        self.slps = [{PosSLP() if pos else SLP()}]
         self.values = {1}
         self.current_size = 1
+        self.pos = pos
 
     def get_all_next_slps(self, slp):
         all_next_slps = []
         for i in range(len(slp)):
             for j in range(len(slp)):
-                next_slps = [deepcopy(slp) for _ in range(3)]
+                next_slps = [deepcopy(slp) for _ in range(2 if self.pos else 3)]
                 next_slps[0].add(Line(i, j, '*'))
                 next_slps[1].add(Line(i, j, '+'))
-                next_slps[2].add(Line(i, j, '-'))
+                if not self.pos:
+                    next_slps[2].add(Line(i, j, '-'))
                 all_next_slps += next_slps
         return all_next_slps
 
